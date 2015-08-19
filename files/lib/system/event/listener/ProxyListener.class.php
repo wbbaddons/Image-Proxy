a110080473ad9f475fc8cb98e963e641d41b58df
Morik <Sebastian.Zimmer@gmx.net>
2015-08-19 12:50:11 +0200

    Fix missing whitespace

--- files/lib/system/event/listener/ProxyListener.class.php
+++ files/lib/system/event/listener/ProxyListener.class.php
@@ -58,7 +58,7 @@ class ProxyListener implements \wcf\system\event\listener\IParameterizedEventLis
 				}
 				else if(\wcf\system\application\ApplicationHandler::getInstance()->isInternalURL($img['attributes'][0]) && $url['scheme']=='http' && \wcf\system\request\RouteHandler::secureConnection()){
 					$protocolRegex = new \wcf\system\regex('^http(?=://)');
-					$img['attributes'][0] =$protocolRegex->replace($img['attributes'][0], 'https');
+					$img['attributes'][0] = $protocolRegex->replace($img['attributes'][0], 'https');
 					$eventObj->message = \wcf\util\StringUtil::replaceIgnoreCase($img['match'], '[img=\''. $img['attributes'][0] .'\''. ((isset($img['attributes'][1])) ? ','.$img['attributes'][1] .((isset($img['attributes'][2])) ? ','.$img['attributes'][2] : ''): '') .'][/img]', $eventObj->message);
 				}
 			}
