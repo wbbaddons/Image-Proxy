99fdbb89fb45250e2106e8109829ccc6d4f19ac7
Morik <Sebastian.Zimmer@gmx.net>
2015-09-09 15:17:17 +0200

    Add "none" as valid orientation option

--- files/lib/system/event/listener/ProxyListener.class.php
+++ files/lib/system/event/listener/ProxyListener.class.php
@@ -25,11 +25,11 @@ class ProxyListener implements \wcf\system\event\listener\IParameterizedEventLis
 		// match [img]link[/img]
 		preg_match_all('~\[img\]([^\]]*)\[\/img\]~i', $eventObj->message, $matches, PREG_SET_ORDER);
 		
-		// match all [img=link,(left|right|center),[0-9]+]
-		preg_match_all("~\[img=\'([^\]]*)\',(left|right|center),([0-9]+)\]\[\/img\]~i", $eventObj->message, $matches2, PREG_SET_ORDER);
+		// match all [img=link,(none|left|right|center),[0-9]+]
+		preg_match_all("~\[img=\'([^\]]*)\',(none|left|right|center),([0-9]+)\]\[\/img\]~i", $eventObj->message, $matches2, PREG_SET_ORDER);
 		
-		// match all [img=link,(left|right|center)]
-		preg_match_all("~\[img=\'([^\]]*)\',(left|right|center)\]\[\/img\]~i", $eventObj->message, $matches3, PREG_SET_ORDER);
+		// match all [img=link,(none|left|right|center)]
+		preg_match_all("~\[img=\'([^\]]*)\',(none|left|right|center)\]\[\/img\]~i", $eventObj->message, $matches3, PREG_SET_ORDER);
 
 		// match [img=link]
 		preg_match_all("~\[img=\'?([^\,\]]*)\'?\]~i", $eventObj->message, $matches4, PREG_SET_ORDER);
